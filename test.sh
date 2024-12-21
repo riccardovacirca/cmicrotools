a0777d399c6442a98ac53aac8d19cd4e2faf4525
riccardovacirca <rvacirca23@gmail.com>
2024-12-21 19:43:50 +0100

    updated

--- test.sh
+++ test.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# URL del servizio
 URL="http://localhost:2310/api/helloworld"
-
-echo "Eseguo il test sull'endpoint $URL..."
-
-# Effettua una richiesta HTTP GET all'endpoint
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-# Verifica il codice di risposta
-if [ "$RESPONSE" -eq 200 ]; then
-  echo "Test superato: il servizio ha risposto con HTTP 200"
-  exit 0
-else
-  echo "Test fallito: il servizio ha risposto con HTTP $RESPONSE"
+if [ "$RESPONSE" -ne 200 ]; then
   exit 1
 fi
